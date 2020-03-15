a1c1a0eb7acaeb38aa1060fe59aeb6b88109b33d
Alexlogvin2019 <alexlogvin@yahoo.com>
2020-03-15 14:15:34 +0200

    Updates

---
+++ precache-manifest.703442ec68b857e24e13e87b58394611.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bf38ebc4e3837029e7a13c3ae1c3363",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "8b4c230c3dd15ea1b280",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.bbe38be4.chunk.css"
+  },
+  {
+    "revision": "b64facf239ae15e51867",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.75eb3b07.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.75eb3b07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b4c230c3dd15ea1b280",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.6e5992fb.chunk.js"
+  },
+  {
+    "revision": "4992c48e99877600891f",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.ad90512f.js"
+  }
+]);
